379a3a3a28ee046c6c336b2b8e69f4029473be69
Tommy Nguyen <tommy@p150hm.(none)>
2012-05-08 11:48:50 -0400

    fix camera libs and add c-flags

--- Android.mk
+++ Android.mk
@@ -4,19 +4,25 @@ include $(CLEAR_VARS)
 
 ifneq (,$(findstring $(TARGET_BOARD_PLATFORM),qsd8k msm7k))
 
+LOCAL_C_FLAGS          += -O3
 LOCAL_MODULE_TAGS      := optional
 LOCAL_MODULE_PATH      := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE           := camera.$(TARGET_BOARD_PLATFORM)
 
 LOCAL_SRC_FILES        := QcomCamera.cpp
 
-LOCAL_SHARED_LIBRARIES := liblog libutils libcamera_client libbinder \
-                          libcutils libhardware libcamera
+LOCAL_SHARED_LIBRARIES := liblog libdl libutils libcamera_client libbinder \
+                          libcutils libhardware libui libcamera
 
 LOCAL_C_INCLUDES       := frameworks/base/services \
                           frameworks/base/include \
-                          hardware/libhardware/include \
-                          hardware/qcom/display/libgralloc
+                          hardware/libhardware/include
+ifeq ($(TARGET_BOARD_PLATFORM),msm7k)
+LOCAL_C_INCLUDES       += hardware/libhardware/modules/gralloc
+LOCAL_CFLAGS           := -DPREVIEW_MSM7K
+else
+LOCAL_C_INCLUDES       += hardware/qcom/display/libgralloc
+endif
 
 LOCAL_PRELINK_MODULE   := false
 
